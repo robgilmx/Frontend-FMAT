d205b190badca5bfc20face9272c218a6910f1db
Roberto Gil <robgilmx@gmail.com>
2018-05-21 00:23:14 -0500

    Commit incial, rama licenciaturas. Implementado: * Licenciatura de Ingeneria de Software. Incluye: * Menú * Lector PDF * CSS * Banner * Archivos PDF * Información real
    Autor: Roberto Gil.
    :joy::ok_hand::point_right::fire:

---
+++ js/lis.js
@@ -0,0 +1,16 @@
+window.onload = (function () {
+   loadHeader();
+});
+
+function loadHeader(){
+    $('.degrees-container').load("banner.html");
+    $('.degrees-menu').load("menu.html");
+}
+
+
+
+function setNavbarActive() {
+    var hrefPage = (location.pathname.substring(location.pathname.lastIndexOf("/") + 1));
+    $('a[href="' + hrefPage + '"]').closest('li').addClass('active');
+}
+
